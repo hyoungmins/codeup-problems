1f73b6338602863e61e36ca378021ca544845cb7
Hyoungmins <pinks1226@naver.com>
2022-11-15 16:49:04 +0900

    problem1001-problem1005

---
+++ basics/Problem1002.java
@@ -0,0 +1,7 @@
+
+public class Problem1002 {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World");
+	}
+}
---
+++ basics/Problem1004.java
@@ -0,0 +1,7 @@
+
+public class Problem1004 {
+
+	public static void main(String[] args) {
+		System.out.println("\'Hello\'");
+	}
+}
