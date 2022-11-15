4b38796ac9f7755feb53456f59b0025448441a45
Hyoungmins <pinks1226@naver.com>
2022-11-15 16:51:33 +0900

    problem1006-problem1010

---
+++ basics/Problem1010.java
@@ -0,0 +1,13 @@
+import java.util.Scanner;
+
+public class Problem1010 {
+
+	public static void main(String[] args) {
+		Scanner sc = new Scanner(System.in);
+		
+		int P1010 = sc.nextInt();
+		System.out.println(P1010);
+
+	}
+
+}
